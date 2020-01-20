26db8c04c93793a8b0d416cbb3e5175cfb4896c6
David Wright <dontbothertheking@gmail.com>
2020-01-20 15:03:22 -0600

    Add login page

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,18 @@ const routes = [
     component: Home
   },
   {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue")
+  },
+  {
     path: "/about",
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-btn block @click="oauth">
+      Log in with Twitch
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    oauth() {
+
+    }
+  }
+};
+</script>
+
+<style>
+</style>
