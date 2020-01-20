43a63e967dd62694caa731c377f86f49a32d9da6
David Wright <dontbothertheking@gmail.com>
2020-01-20 16:54:20 -0600

    Add client id to headers

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export default function install(Vue) {
   axios.defaults.baseURL = "https://api.twitch.tv/helix";
+  axios.defaults.headers = {
+    "Client-ID": process.env.VUE_APP_CLIENT_ID
+  };
 
   Vue.prototype.$get = resourse => axios.get(resourse);
 
