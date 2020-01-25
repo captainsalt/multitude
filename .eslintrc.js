003a1285141ecbd607fd9a8e45cbdb74931488e7
David Wright <dontbothertheking@gmail.com>
2020-01-24 18:12:15 -0600

    Change eol to clrf

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   "rules": {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "indent": [1, 2, { SwitchCase: 1 }]
+    "indent": [1, 2, { SwitchCase: 1 }],
+    "linebreak-style": [1, "windows"]
   },
   "parserOptions": {
     parser: "babel-eslint"
