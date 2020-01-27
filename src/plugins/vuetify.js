d69736b2ec52f80abf4355e751123c0d992f0a39
David Wright <dontbothertheking@gmail.com>
2020-01-27 12:02:17 -0600

    Default to dark mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    primary: colors.purple.darken3,
+    dark: true,
+    themes: {
+      light: {
+      },
+      dark: {
+      }
+    }
+  }
 });
