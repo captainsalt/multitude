cc421f317d9bdac2bd113aec1f713577a2708076
David Wright <dontbothertheking@gmail.com>
2020-01-27 12:22:21 -0600

    Add themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,17 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-    primary: colors.purple.darken3,
     dark: true,
     themes: {
       light: {
+        primary: "#9147ff"
       },
       dark: {
+        primary: "#9147ff"
       }
     }
   }
