4e0fb98f22f59b8df3d7d7b7e61e19636670d454
David Wright <dontbothertheking@gmail.com>
2020-01-26 21:19:47 -0600

    Add user state to auth

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,14 +2,32 @@ export default {
   namespaced: true,
   state: {
     accessToken: "",
-    idToken: ""
+    idToken: "",
+    userId: "",
+    username: "",
+    pictureUrl: ""
   },
   getters: {
     getAccessToken: state => state.accessToken,
-    getIdToken: state => state.idToken
+    getIdToken: state => state.idToken,
+    getUsername: state => state.username,
+    getUserId: state => state.userId,
+    getPictureUrl: state => state.getPictureUrl
   },
   mutations: {
     setAccessToken: (state, payload) => state.accessToken = payload,
-    setIdToken: (state, payload) => state.idToken = payload
+    setIdToken: (state, payload) => state.idToken = payload,
+    setUsername: (state, payload) => state.username = payload,
+    setUserId: (state, payload) => state.userId = payload,
+    setPicureUrl: (state, payload) => state.pictureUrl = payload
+  },
+  actions: {
+    setUser({ commit }, userObject) {
+      const { sub: id, preferred_username, picture } = userObject;
+
+      commit("setIdToken", id);
+      commit("setUsername", preferred_username);
+      commit("setPicureUrl", picture);
+    }
   }
 };
